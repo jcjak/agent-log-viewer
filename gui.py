30feb630609b494651106c77497eccce8d8b5dca
Jason Katz <jason.katz@jumpcloud.com>
2019-03-25 10:58:11 -0600

    latest build

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import re
 
 #class JCLogView():
     
@@ -11,17 +13,22 @@ app.setOrganizationName('JumpCloud')
 
 # Code to display log files after import
 editor = QTextEdit()
+editor.setMinimumSize(800, 600)
 editor.setAcceptDrops(True)
-
 editor.setReadOnly(True)
 editor.setText('Test')
 
-# Some test code
+lineeditor = QLineEdit()
+
+# Sets the window, layout, and button.
 window = QWidget()
 layout = QVBoxLayout()
 button = QPushButton('Add Log File')
 
 layout.addWidget(button)
+
+# Instantiating the syntax highlighter module to color the text
+highlighter = QTextCharFormat()
 
 # Log text display output
 layout.addWidget(editor)
@@ -32,20 +39,18 @@ window.show()
 def on_button_click(self):
     dialog = QFileDialog()
     fname = dialog.getOpenFileName(None, 'Open Log', '/')
-    color = QColorDialog.getColor(255, 0, 0)
+    color = QColor(255,0,0)
 
     if fname[0]:
             f = open(fname[0], 'r')
 
             with f:
                 data = f.read()
-                editor.setText(data)
-                
-                #num_lines = sum(1 for line in output)
-                #line_count = f.readline(num_lines)
-
+                    
+                # this code works but it colors the entire log.
+                # trying to figure out how to get it to color a single line only
                 if '[ERROR]' in data:
-                        editor.setTextColor(color)
+                        editor.setStyleSheet('color: red')
 
 
 # Execute the app indefinitely, until user terminates process/app.
